cfa09ce5b6c202ad664e08fd9f0c8bcfd248b196
bayler <owawa26@gmail.com>
2022-11-16 20:20:30 +0300

    minor

--- components/Product.js
+++ components/Product.js
@@ -24,7 +24,7 @@ function Product({ id, title, price, description, category, image }) {
             {Array(rating)
                 .fill()
                 .map((_, i) => (
-                <StarIcon className="h-5 text-yellow-500"/>
+                <StarIcon key={i} className="h-5 text-yellow-500"/>
             ))}
         </div>
 
